ee4feaa3e3db57d923fd4202353329b651d646c8
Guillaume EGRET <guegret@gmail.com>
2023-01-08 20:10:20 +0100

    changed the parameters of the API call

--- app/request/PokedexRequest.tsx
+++ app/request/PokedexRequest.tsx
@@ -3,7 +3,8 @@ import fetch from 'node-fetch';
 
 const API_URL = 'https://pokeapi.co/api/v2/';
 
-async function fetchAllPokemons(limit:number = 500) {
+//Set the limit to 100 because this is just a test app, we don't really need this much data
+async function fetchAllPokemons(limit:number = 100) {
     return await fetch(`${API_URL}pokemon?limit=${limit}`)
         .then((response: { json: () => any; }) => response.json());
 }
