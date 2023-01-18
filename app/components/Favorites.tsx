c87c02dc7a66606225c2c5cd1fc2d60711aba2b5
Guillaume EGRET <guegret@gmail.com>
2023-01-18 19:19:50 +0100

    added svg stars in pokedex in each pokemon item

--- app/components/Favorites.tsx
+++ app/components/Favorites.tsx
@@ -9,32 +9,7 @@ interface Pokemon {
 const Favorites = () => {
     const [isLoading, setLoading] = useState(true);
     const [data, setData] = useState<Array<Pokemon>>([]);
-    return (
-    <SafeAreaView style={styles.container}>
-        <ScrollView style={styles.scrollView}>
-            {isLoading ? <Text>Not yet implemented...</Text> :
-                data.map(elem => {
-                    console.log(elem);
-                    return <Text key={elem.name}>{elem.name}</Text>
-                })
-            }
-        </ScrollView>
-    </SafeAreaView>
-    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: StatusBar.currentHeight,
-    backgroundColor: 'aliceblue',
-  },
-  scrollView: {
-    marginHorizontal: 20,
-  },
-  text: {
-    fontSize: 42,
-  },
-});
 
 export default Favorites;
