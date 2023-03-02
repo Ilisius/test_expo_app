d21137ca5df64ec4d6a641c37580c157676cb0c7
Guillaume Egret <guillaumeeg@bam.tech>
2023-03-02 15:50:35 +0100

    added search bar in Pokedex

--- app/components/Pokedex.tsx
+++ app/components/Pokedex.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, SafeAreaView, ScrollView, View } from "react-native";
 import { usePokedex } from "../hooks/usePokedex";
 import { AnimatedStar } from "./AnimatedStar";
 import { PokemonItem } from "./PokemonItem";
+import { SearchBar } from "@rneui/themed";
 
 export interface Pokemon {
   name: string;
@@ -13,6 +14,7 @@ export const Pokedex = () => {
   const { isLoading, dataPokemon } = usePokedex();
   return (
     <SafeAreaView style={styles.container}>
+      <SearchBar lightTheme />
       <ScrollView style={styles.scrollView}>
         {isLoading ? (
           <Text>Loading Pokedex...</Text>
