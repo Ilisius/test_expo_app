1c0228ad2a961384e88a2a9aca7914c23e2418fa
Guillaume Egret <guillaumeeg@bam.tech>
2023-03-02 15:12:39 +0100

    just eslint doing stuff

--- app/screens/FavoritesPage.tsx
+++ app/screens/FavoritesPage.tsx
@@ -1,18 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Favorites from '../components/Favorites';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Favorites } from "../components/Favorites";
 
 export default function FavoritesPage() {
-  return (
-    <Favorites/>
-  );
+  return <Favorites />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
