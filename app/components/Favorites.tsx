7045f3addbb2614da3e1c7bec5b37dc9371b174b
Guillaume Egret <guillaumeeg@bam.tech>
2023-03-02 10:46:19 +0100

    extracted all functions and logic from Favorites to a hook

--- app/components/Favorites.tsx
+++ app/components/Favorites.tsx
@@ -1,59 +1,62 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, SafeAreaView, ScrollView, StatusBar, View } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
-import AnimatedStar from './AnimatedStar';
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  View,
+} from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import { useFavorites } from "../hooks/useFavorites";
+import AnimatedStar from "./AnimatedStar";
 
 interface Pokemon {
-    name : string;
-    apiUrl : string;
+  name: string;
+  apiUrl: string;
 }
 
 const Favorites = () => {
-    const [isLoading, setLoading] = useState(true);
-    const [data, setData] = useState<Array<Pokemon>>([]);
-    const favoritesPokemons = useSelector((state : {favorite : Array<Pokemon>}) => state.favorite);
-    useEffect(() => {
-        setData(favoritesPokemons);
-        setLoading(false);
-    });
-    return (
+  const { isLoading, dataPokemon } = useFavorites();
+  return (
     <SafeAreaView style={styles.container}>
-        <ScrollView style={styles.scrollView}>
-            {isLoading ? <Text>Loading Pokedex...</Text> :
-                data.map(pokemon => {
-                    return (
-                        <View key={pokemon.apiUrl} style={styles.pokemonItem}>
-                            <Text style={styles.pokemonText} >{pokemon.name}</Text>
-                            <AnimatedStar pokemon={pokemon}/>
-                        </View>)
-                })
-            }
-        </ScrollView>
+      <ScrollView style={styles.scrollView}>
+        {isLoading ? (
+          <Text>Loading Pokedex...</Text>
+        ) : (
+          dataPokemon.map((pokemon) => {
+            return (
+              <View key={pokemon.apiUrl} style={styles.pokemonItem}>
+                <Text style={styles.pokemonText}>{pokemon.name}</Text>
+                <AnimatedStar pokemon={pokemon} />
+              </View>
+            );
+          })
+        )}
+      </ScrollView>
     </SafeAreaView>
-    );
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'aliceblue',
-    },
-    scrollView: {
-        marginHorizontal: 20,
-    },
-    pokemonItem: {
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        marginBottom: 5,
-        marginTop: 5,
-        alignItems: "center",
-    },
-    pokemonText: {
-        fontSize: 25,
-    },
-
+  container: {
+    flex: 1,
+    backgroundColor: "aliceblue",
+  },
+  scrollView: {
+    marginHorizontal: 20,
+  },
+  pokemonItem: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 5,
+    marginTop: 5,
+    alignItems: "center",
+  },
+  pokemonText: {
+    fontSize: 25,
+  },
 });
-
 
 export default Favorites;
