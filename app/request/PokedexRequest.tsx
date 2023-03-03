118451aedab2bd65fbe83cfe5e12f2dafe6fd306
Guillaume Egret <guillaumeeg@bam.tech>
2023-03-03 15:48:16 +0100

    refactor: changed declarations of function from : function f() {...} to : const f = () => {...}

--- app/request/PokedexRequest.tsx
+++ app/request/PokedexRequest.tsx
@@ -5,12 +5,12 @@ import { err } from "react-native-svg/lib/typescript/xml";
 const API_URL = "https://pokeapi.co/api/v2/";
 
 //Set the limit to 100 because this is just a test app, we don't really need this much data
-async function fetchAllPokemons(limit: number = 100) {
+const fetchAllPokemons = async (limit: number = 100) => {
   const results = fetch(`${API_URL}pokemon?limit=${limit}`);
   const apiResults = (await results).json();
   return apiResults;
-}
-async function fetchPokemonsByName(name: string) {
+};
+const fetchPokemonsByName = async (name: string) => {
   try {
     const results = fetch(`${API_URL}pokemon/${name}`);
     const apiResults = (await results).json();
@@ -18,6 +18,6 @@ async function fetchPokemonsByName(name: string) {
   } catch (error) {
     return;
   }
-}
+};
 
 export { fetchAllPokemons, fetchPokemonsByName };
