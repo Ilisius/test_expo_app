751666ddd2641d2be1aca11708aa2bb2f49aa326
Guillaume Egret <guillaumeeg@bam.tech>
2023-03-03 15:59:38 +0100

    refactor: changed location of await in both functions of PokedexRequest

--- app/request/PokedexRequest.tsx
+++ app/request/PokedexRequest.tsx
@@ -5,14 +5,14 @@ const API_URL = "https://pokeapi.co/api/v2/";
 
 //Set the limit to 100 because this is just a test app, we don't really need this much data
 const fetchAllPokemons = async (limit: number = 100) => {
-  const results = fetch(`${API_URL}pokemon?limit=${limit}`);
-  const apiResults = (await results).json();
+  const results = await fetch(`${API_URL}pokemon?limit=${limit}`);
+  const apiResults = results.json();
   return apiResults;
 };
 const fetchPokemonsByName = async (name: string) => {
   try {
-    const results = fetch(`${API_URL}pokemon/${name}`);
-    const apiResults = (await results).json();
+    const results = await fetch(`${API_URL}pokemon/${name}`);
+    const apiResults = results.json();
     return apiResults;
   } catch (error) {
     return;
