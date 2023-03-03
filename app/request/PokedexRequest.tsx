f422abb5814a8bb4ca78dc44464c68ae9deb6d8c
Guillaume Egret <guillaumeeg@bam.tech>
2023-03-03 11:39:52 +0100

    refacto PokedexRequest to be more understandable

--- app/request/PokedexRequest.tsx
+++ app/request/PokedexRequest.tsx
@@ -1,13 +1,14 @@
 //Needed for jest's test
 import fetch from "node-fetch";
+import { err } from "react-native-svg/lib/typescript/xml";
 
 const API_URL = "https://pokeapi.co/api/v2/";
 
 //Set the limit to 100 because this is just a test app, we don't really need this much data
 async function fetchAllPokemons(limit: number = 100) {
-  return await fetch(`${API_URL}pokemon?limit=${limit}`).then(
-    (response: { json: () => any }) => response.json()
-  );
+  const results = fetch(`${API_URL}pokemon?limit=${limit}`);
+  const apiResults = (await results).json();
+  return apiResults;
 }
 async function fetchPokemonsByName(name: string) {
   try {
@@ -15,7 +16,6 @@ async function fetchPokemonsByName(name: string) {
     const apiResults = (await results).json();
     return apiResults;
   } catch (error) {
-    console.log(error);
     return;
   }
 }
